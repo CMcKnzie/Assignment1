1a6692b17d3d32b475105e8a5a84a2c3e802c0ef
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-09-08 18:33:03 -0500

    First upload

---
+++ scripts/myScripts.js
@@ -0,0 +1,23 @@
+var varTime;
+var timer = document.getElementById("time");
+var countDownSecs;
+
+function startTime(){ 
+  varTime = setInterval(start, 1000);
+  document.getElementById("timerDisplay").innerHTML = timer.value;
+  countDownSecs = timer.value;
+} 
+
+function start(){
+  countDownSecs--;
+  document.getElementById("timerDisplay").innerHTML = countDownSecs;
+  if (countDownSecs == -1){
+    stop();
+    document.getElementById("timerDisplay").innerHTML = "0";
+    alert("Timer Finished!")
+  }
+}
+
+function stop(){
+  clearInterval(varTime);
+}
